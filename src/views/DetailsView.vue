57d4be16342d46ec4ffe708839e9b76d356a7fde
John <vannok@live.com>
2022-04-04 02:30:49 +0300

    commit

---
+++ src/views/DetailsView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="post" class="post">
+    <h3>{{ post.title }} - {{ post.id }}</h3>
+    <p class="pre">
+      {{ post.body }}
+    </p>
+  </div>
+</template>
+
+<script>
+import getPost from "../composables/getPost";
+export default {
+  props: ["id"],
+  setup(props) {
+    const { post, load } = getPost(props.id);
+    load();
+    return { post };
+  },
+};
+</script>
+
+<style>
+.post {
+  max-width: 600px;
+  margin: 0.5rem auto;
+  border: 2px solid #444;
+}
+.post p {
+  color: #444;
+  line-height: 1.5em;
+  margin-top: 40px;
+}
+.pre {
+  white-space: pre-wrap;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <PostList v-if="posts.length" :posts="posts" />
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import PostList from "../components/PostList.vue";
+import getPosts from "../composables/getPosts";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: { PostList },
+  setup() {
+    const { posts, load } = getPosts();
+    load();
+    return { posts };
+  },
+};
 </script>
